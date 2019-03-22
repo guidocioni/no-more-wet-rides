Fixed memory leak by converting units only at the end

radar_forecast_bike.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import sys
 import utils
+import radolan as radar
 
 from pathlib import Path
 
@@ -10,6 +11,8 @@ shifts = (1, 3, 5, 7, 9)
 # Folder to download the data (they will be removed 
 # but it needs some space to start with)
 data_path = Path("/tmp")
+data_path.mkdir(exist_ok=True)
+
 json = True 
 
 def make_plot(time_radar, rain_bike, dtime_bike, out_filename=None):
@@ -72,7 +75,11 @@ def extract_rain_rate_from_radar(time_radar, dtime_radar, lon_bike, lat_bike, ti
             # Finally append the subsetted value to the array
             temp.append(rr[ind_time+shift, indx, indy])
         # iterate over all the shifts
-        rain_bike=np.append(rain_bike, [temp], axis=0)
+        rain_bike = np.append(rain_bike, [temp], axis=0)
+        # convert to mm/h now on the smaller array, this was previously done in
+        # utils.py but was causing more memory usage
+        rain_bike = rain_bike/2. -32.5 #dbz
+        rain_bike = radar.z_to_r(radar.idecibel(rain_bike), a=256, b=1.42) # mm/h
 
     return rain_bike
 
@@ -94,7 +101,6 @@ def main(track_file, plot_filename='plot.png'):
 
     make_plot(time_radar=time_radar, rain_bike=rain_bike, dtime_bike=dtime_bike,
               out_filename=plot_filename)
-
 
 if __name__ == "__main__":
     if not sys.argv[1:]:

utils.py
@@ -96,8 +96,6 @@ def get_radar_data(data_path, remove_file=False):
     the local and remote file. This should work in most of the cases
     but it's not 100% correct.'''
 
-    data_path.mkdir(exist_ok=True)
-
     radar_fn = data_path/'FX_LATEST.tar.bz2'
 
     if not radar_fn.exists():
@@ -154,10 +152,12 @@ def process_radar_data(fnames, remove_file):
         for fname in fnames:
             os.remove(fname)
 
-    # Convert to a masked array and use the right units
+    # Convert to a masked array
+    # The conversion for mm/h is done afterwards to avoid memory usage
     data = np.ma.array(data)
-    dbz  = data/2. - 32.5 
-    rr   = radar.z_to_r(radar.idecibel(dbz), a=256, b=1.42) #mm/h
+    # dbz  = data/2. - 32.5 
+    # rr   = radar.z_to_r(radar.idecibel(dbz), a=256, b=1.42) #mm/h THIS IS CAUSING THE MEMORY LEAK <----------
+    rr = data
 
     # Get coordinates (space/time)
     lon_radar, lat_radar = radar.get_latlon_radar()