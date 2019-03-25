Fixed a bug in the computation of rain rate Removed time_bike from function as it is not used Added function to convert lat/lon to km Removed usage of mask arrays

radar_forecast_bike.py
@@ -25,7 +25,7 @@ def make_plot(time_radar, rain_bike, dtime_bike, out_filename=None):
     ax = plt.gca()
 
     # Create the labels including the original datetime and a sum of the rain
-    deltas_string   = [delta.strftime('%H:%M') for delta in np.array(time_radar)[np.array(shifts)]]
+    deltas_string   = [delta.strftime('%H:%M') for delta in time_radar[np.array(shifts)]]
     sums_string     = ['%4.2f mm' % value for value in rain_bike.sum(axis=1)*(5./60.)]
     labels          = ['start '+m+', tot. '+n for m,n in zip(deltas_string, sums_string)]
     # Since timedelta objects are not correctly handled by matplotlib
@@ -55,8 +55,10 @@ def make_plot(time_radar, rain_bike, dtime_bike, out_filename=None):
     else:
         plt.show(block=True)
 
+    return(fig)
 
-def extract_rain_rate_from_radar(time_radar, dtime_radar, lon_bike, lat_bike, time_bike, dtime_bike, lon_radar, lat_radar, rr):
+
+def extract_rain_rate_from_radar(time_radar, dtime_radar, lon_bike, lat_bike, dtime_bike, lon_radar, lat_radar, rr):
     # Compute the rain at the bike position
     rain_bike=np.empty(shape=(0, len(dtime_bike))) # Initialize the array
 
@@ -69,38 +71,42 @@ def extract_rain_rate_from_radar(time_radar, dtime_radar, lon_bike, lat_bike, ti
             # the comparison quite easy!
             ind_time = np.argmin(np.abs(dtime_radar - dtime_b))
             # Find also the closest point in space between radar and the
-            # track from the bike
+            # track from the bike. Would be nice to compute the distance in km
+            # using utils.distance_km but this would be too slow!
             dist = np.sqrt((lon_radar-lon_b)**2+(lat_radar-lat_b)**2)
-            indx, indy=np.unravel_index(np.argmin(dist, axis=None), dist.shape)
+            indx, indy = np.unravel_index(np.argmin(dist, axis=None), dist.shape)
             # Finally append the subsetted value to the array
             temp.append(rr[ind_time+shift, indx, indy])
         # iterate over all the shifts
         rain_bike = np.append(rain_bike, [temp], axis=0)
-        # convert to mm/h now on the smaller array, this was previously done in
-        # utils.py but was causing more memory usage
-        rain_bike = rain_bike/2. -32.5 #dbz
-        rain_bike = radar.z_to_r(radar.idecibel(rain_bike), a=256, b=1.42) # mm/h
+
+    # convert to mm/h now on the smaller array, this was previously done in
+    # utils.py but was causing more memory usage
+    rain_bike = rain_bike/2. -32.5 #dbz
+    rain_bike = radar.z_to_r(radar.idecibel(rain_bike), a=256, b=1.42) # mm/h
 
     return rain_bike
 
 
 def main(track_file, plot_filename='plot.png'):
-    lon_bike, lat_bike, time_bike, dtime_bike = utils.read_input(track_file)
+    lon_bike, lat_bike, dtime_bike = utils.read_input(track_file)
 
     lon_radar, lat_radar, time_radar, dtime_radar, rr = utils.get_radar_data(data_path)
 
     rain_bike = extract_rain_rate_from_radar(time_radar=time_radar,
             lon_bike=lon_bike, lat_bike=lat_bike, dtime_bike=dtime_bike,
-            time_bike=time_bike, dtime_radar=dtime_radar, lat_radar=lat_radar,
+            dtime_radar=dtime_radar, lat_radar=lat_radar,
             lon_radar=lon_radar, rr=rr)
 
     # convert to JSON
     if json:
-        deltas_string   = [delta.strftime('%H:%M') for delta in np.array(time_radar)[np.array(shifts)]]
+        deltas_string   = [delta.strftime('%H:%M') for delta in time_radar[np.array(shifts)]]
         utils.convert_to_json(rain_bike, dtime_bike, deltas_string, url='data.json')
 
-    make_plot(time_radar=time_radar, rain_bike=rain_bike, dtime_bike=dtime_bike,
+    fig = make_plot(time_radar=time_radar, rain_bike=rain_bike, dtime_bike=dtime_bike,
               out_filename=plot_filename)
+
+    return(fig)
 
 if __name__ == "__main__":
     if not sys.argv[1:]:

utils.py
@@ -36,7 +36,7 @@ def read_input(track_file):
     # the maximum resolution that we can achieve... 
     dtime_bike = time_bike - time_bike[0]
 
-    return lon_bike, lat_bike, time_bike, dtime_bike
+    return lon_bike, lat_bike, dtime_bike
 
 def gpx_parser(track_file):
     '''Parse lat, lon and time from a gpx file.
@@ -54,6 +54,27 @@ def gpx_parser(track_file):
                 lon.append(point.longitude)
                 time.append(point.time.replace(tzinfo=None))
     return(np.array(lon), np.array(lat), pd.to_datetime(time))
+
+def distance_km(lon1, lon2, lat1, lat2):
+	'''Returns the distance (in km) between two array of points'''
+	radius = 6371 # km
+
+	dlat = np.deg2rad(lat2-lat1)
+	dlon = np.deg2rad(lon2-lon1)
+	a = np.sin(dlat/2) * np.sin(dlat/2) + np.cos(np.deg2rad(lat1)) \
+	    * np.cos(np.deg2rad(lat2)) * np.sin(dlon/2) * np.sin(dlon/2)
+	c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+	d = radius * c
+
+	return d
+
+def distance_bike(lon_bike, lat_bike):
+	'''Finds the distance (in km) from the starting point of the bike
+	track.'''
+	lon_bike_shift = np.roll(lon_bike, -1)[:-2] # Avoid taking the last point
+	lat_bike_shift = np.roll(lat_bike, -1)[:-2]
+
+	return(distance_km(lon_bike_shift, lon_bike[:-2], lat_bike_shift, lat_bike[:-2]).cumsum())
 
 def _utc_to_local(utc_dt):
     """Convert UTC time to local time"""
@@ -139,13 +160,13 @@ def process_radar_data(fnames, remove_file):
     '''From the names of the files return arrays where the data is 
     concatenated. '''
     data = []
-    datestring = []
+    time_radar = []
 
     for fname in fnames:
         rxdata, rxattrs = radar.read_radolan_composite(fname)
-        data.append(np.ma.masked_equal(rxdata, -9999))
+        data.append(rxdata)
         minute = int(RADAR_FILENAME_REGEX.match(fname.name)['minutes'])
-        datestring.append((rxattrs['datetime']+timedelta(minutes=minute)))
+        time_radar.append((rxattrs['datetime']+timedelta(minutes=minute)))
 
     if remove_file:
         # Remove the files since we don't need them anymore
@@ -153,17 +174,22 @@ def process_radar_data(fnames, remove_file):
             os.remove(fname)
 
     # Convert to a masked array
-    # The conversion for mm/h is done afterwards to avoid memory usage
-    data = np.ma.array(data)
-    # dbz  = data/2. - 32.5 
-    # rr   = radar.z_to_r(radar.idecibel(dbz), a=256, b=1.42) #mm/h THIS IS CAUSING THE MEMORY LEAK <----------
+    # The conversion to mm/h is done afterwards to avoid memory usage
+    data = np.array(data)
+    # Get rid of masking value, we have to check whether this cause problem
+    # In this case missing data is treated as 0 (no precip.). Masked arrays
+    # cause too many problems. 
+    data[data==-9999] = 0.
     rr = data
 
     # Get coordinates (space/time)
     lon_radar, lat_radar = radar.get_latlon_radar()
-    dtime_radar          = pd.to_datetime(datestring)-pd.to_datetime(datestring[0])
+    time_radar 	= pd.to_datetime(time_radar)
+    dtime_radar = time_radar - time_radar[0]
     # dtime_radar is a timedelta object! 
 
-    time_radar = np.array([_utc_to_local(s) for s in  datestring])
+    #time_radar = np.array([_utc_to_local(s) for s in  datestring])
+    # Until I can fix the timezone conversion which should not depend on calendar 
+    
 
     return lon_radar, lat_radar, time_radar, dtime_radar, rr